e8e3352d03424be3ef42ff839540b631e8dad5f3
dougy <dougyherard@gmail.com>
2019-01-22 14:33:07 -0500

    Added brief elements
    All there in the title.

--- assignment-3/js/script.js
+++ assignment-3/js/script.js
@@ -1,37 +1,69 @@
-let gameOver = false;
-
-let time = 0;
+let nbOfBonuses = 0;
+let bonusFound = 0;
 
 $(document).ready(function(){
   $('#restart').hide();
+  nbOfBonuses = $('.unbonus').length;
   setInterval(update, 500);
 
   $('.redacted').on('click', spanClicked);
-  $('.unbonus').hover(revealBonus);
+  $('.unbonus').hover(bonusRevealed);
 });
 
 function update(){
-  console.log(gameOver);
+
   if ($('.redacted').length === 0){
-      console.log("Game over!");
-      $('span').off('click', '.revealed');
-      $('#restart').fadeIn();
+      //console.log("Game over!");
+      $('.revealed').each(spanOff);
+      $('.unbonus').each(bonusOff);
+      $('#restart').fadeIn().on('click', reset);
   }
   else
-    $('span').each(updateSpan);
+    $('span').each(spanUpdate);
 }
 
-function updateSpan(){
+function spanUpdate(){
   r = Math.random();
-  if (r < .5){
+  if (r < .2){
     $(this).removeClass('redacted').addClass('revealed');
   }
 }
 
 function spanClicked(){
-  $(this).removeClass('revealed').addClass('redacted');
+    $(this).removeClass('revealed').addClass('redacted');
 }
 
-function revealBonus(){
+function spanOff(){
+  //console.log("Span turning off");
+  $(this).off('click');
+}
+
+function bonusRevealed(){
+  console.log ($(this).is('.unbonus'));
+  if ($(this).is('.unbonus')){
+    bonusFound++;
+  }
+
   $(this).removeClass('unbonus').addClass('bonus');
+
+  $('#bonus-text').text("Found " + bonusFound + " of " + nbOfBonuses + " bonus words!");
+}
+
+function bonusOff(){
+  $(this).unbind('mouseenter').unbind('mouseleave');
+}
+
+function reset(){
+  $('.revealed').each(function(){
+    $(this).removeClass('revealed').addClass('redacted').on('click', spanClicked);
+  });
+
+  $('.bonus').each(function(){
+    $(this).removeClass('bonus').addClass('unbonus').hover(bonusRevealed);
+  });
+
+  $('#bonus-text').text("");
+  bonusFound = 0;
+
+  $(this).fadeOut();
 }
