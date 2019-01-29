efe595b9f9294f29ab5f851de43d5d00cfe045c7
dougy <dougyherard@gmail.com>
2019-01-29 11:25:30 -0500

    Double update
    Got the project 1 "bubble" div to work (the "type" in css include somehow made it unreadable...weird) and completed exercise 1 Eat Up with a little additional finale.

---
+++ exercise-1/js/script.js
@@ -0,0 +1,75 @@
+let $mouth;
+let $fly;
+
+let maxChew = Math.random() * 30;
+let currChew = 0;
+
+let buzzSound = new Audio("assets/sounds/buzz.mp3");
+let chewingSound = new Audio("assets/sounds/crunch.wav");
+let burpSound = new Audio("assets/sounds/burp.wav");
+let hmmSound = new Audio("assets/sounds/onomatopoeia.wav");
+
+let isChewing;
+
+
+$(document).ready(function(){
+  console.log(maxChew);
+  if (maxChew > 10)
+    maxChew = 10;
+
+  $mouth = $('#mouth');
+  $fly = $('#fly');
+
+  $fly.draggable({
+    start: function(event, ui){
+      buzzSound.play();
+      buzzSound.loop = true;
+    },
+
+    stop: function(event, ui){
+      buzzSound.pause();
+    }
+  });
+
+  $mouth.droppable({
+    over: function(){
+      $mouth.attr("src", "assets/images/mouth-open.png");
+    },
+
+    out: function(){
+      $mouth.attr("src", "assets/images/mouth-closed.png");
+    },
+
+    drop: function(event, ui){
+      ui.draggable.hide();
+      buzzSound.pause();
+      chew();
+      isChewing = setInterval(chew, 250);
+      chewingSound.play();
+      chewingSound.loop = true;
+    }
+  });
+});
+
+function chew(){
+  if ($mouth.attr("src") === "assets/images/mouth-open.png")
+    $mouth.attr("src", "assets/images/mouth-closed.png");
+  else
+    $mouth.attr("src", "assets/images/mouth-open.png");
+
+  console.log(currChew);
+  currChew++;
+  if (currChew >= maxChew){
+    $mouth.attr("src", "assets/images/mouth-open.png");
+
+    chewingSound.pause();
+    burpSound.play();
+
+    burpSound.addEventListener("ended", function(){
+      $mouth.attr("src", "assets/images/mouth-closed.png");
+      hmmSound.play();
+    });
+
+    clearInterval(isChewing);
+  }
+}
