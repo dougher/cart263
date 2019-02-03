4268941b406393c7b0a7d4e2603d571c2514ee0e
dougy <dougyherard@gmail.com>
2019-02-03 18:23:02 -0500

    Renaming
    Reanmed exercise-1 to assignment-4 and worked some on project-1.

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,6 +1,20 @@
 //let bubbles = [];
+let imgs = [];
+imgs.push("assets/images/mooncake_noblink.gif");
+imgs.push("assets/images/mooncake_blinkonce.gif");
+imgs.push("assets/images/mooncake_blinktwice.gif");
+let $img;
+
+let state = {Idle:0, BlinkOnce:1, BlinkTwice:2};
+let current = state.Idle;
 
 $(document).ready(function(){
+  $img = $('#mooncake');
+
+  $img.attr('src', imgs[0]);
+
+  setInterval(randomFace, 1500);
+
   setInterval(update, 2000);
 });
 
@@ -22,9 +36,17 @@ function update(){
     this.remove();
   });
 
+  b.click(function(){
+    this.remove();
+  });
+
   $('body').append(b);
 
   //bubbles.push(b);
+}
 
-
+function randomFace(){
+  let curr = getRandomInt(0, imgs.length - 1);
+  console.log(curr);
+  $img.attr('src', imgs[curr]);
 }
