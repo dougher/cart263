2d2399075b87afa32edb063748793c6230ad7625
dougy <dougyherard@gmail.com>
2019-02-14 20:10:54 -0500

    Update assignment#4
    Added the image and code for the food to be refuse, as well as a few mouth sounds to complete the picture.

--- assignment-4/js/script.js
+++ assignment-4/js/script.js
@@ -2,16 +2,16 @@ let $mouth;
 let $fly;
 let $choco;
 
-let maxChew = Math.random() * 30;
+let maxChew = parseInt(Math.random() * 30);
 let currChew = 0;
+let isChewing = false;
 
 let buzzSound = new Audio("assets/sounds/buzz.mp3");
+let ahhhSound = new Audio("assets/sounds/ahhh.mp3");
 let chewingSound = new Audio("assets/sounds/crunch.wav");
 let burpSound = new Audio("assets/sounds/burp.wav");
-let hmmSound = new Audio("assets/sounds/onomatopoeia.wav");
-
-let isChewing;
-
+let hmmSound = new Audio("assets/sounds/hmmm.wav");
+let nuhuhSound = new Audio("assets/sounds/nuh-uh.mp3");
 
 $(document).ready(function(){
   console.log(maxChew);
@@ -33,22 +33,41 @@ $(document).ready(function(){
     }
   });
 
+  $choco.draggable({revert: "valid"});
+
   $mouth.droppable({
-    over: function(){
-      $mouth.attr("src", "assets/images/mouth-open.png");
+
+    over: function(event, ui){
+
+      if ($choco.attr("id") === ui.draggable.attr("id")){
+        //console.log("Playing nuh-uhhh.");
+        nuhuhSound.play();
+        nuhuhSound.loop = false;
+      } else{
+        $mouth.attr("src", "assets/images/mouth-open.png");
+        ahhhSound.play();
+        ahhhSound.loop = false;
+      }
+
     },
 
     out: function(){
       $mouth.attr("src", "assets/images/mouth-closed.png");
+      ahhhSound.pause();
+      ahhhSound.currentTime = 0;
     },
 
     drop: function(event, ui){
-      ui.draggable.hide();
-      buzzSound.pause();
-      chew();
-      isChewing = setInterval(chew, 250);
-      chewingSound.play();
-      chewingSound.loop = true;
+      if ($fly.attr("id") === ui.draggable.attr("id")){
+        ui.draggable.hide();
+        buzzSound.pause();
+        ahhhSound.pause();
+        
+        chew();
+        isChewing = setInterval(chew, 250);
+        chewingSound.play();
+        chewingSound.loop = true;
+      }
     }
   });
 });
